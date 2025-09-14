57398c2e1b307009d2c0108aa67092043d45dba9
Bernard Siebens <bernard@siebens.org>
2025-09-14 20:45:50 +0200

    Refactor notifications: Rename `EventNotification` to `CalendarNotification`, update template logic for notification type, add admin menu item in navbar, and enhance footer layout.

--- notifications/notifications.py
+++ notifications/notifications.py
@@ -2,7 +2,7 @@ from generic_notifications.types import register, NotificationType
 
 
 @register
-class EventNotification(NotificationType):
-    key: str = "event"
-    name: str = "Event Notifications"
+class CalendarNotification(NotificationType):
+    key: str = "calendar"
+    name: str = "Calendar Notifications"
     description: str = "Notification for events, new event created or event updated."
