8321cb3f742382f5fe70a15db3fba989d6632eb9
Bernard Siebens <bernard@siebens.org>
2025-09-23 19:48:27 +0200

    Adding in dj_database_url

--- ClubManager/settings.py
+++ ClubManager/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from decouple import config
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -106,6 +108,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+DATABASES['default'] = dj_database_url.config(config("DJANGO_DATABASE_URL", default="sqlite:///db.sqlite3"), str))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
