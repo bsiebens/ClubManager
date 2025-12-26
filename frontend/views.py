6f29e7ec7e531ee8f47640941906730647e72197
Bernard Siebens <bernard@siebens.org>
2025-12-26 23:41:03 +0100

    Added conversations listing and rendering functionality with unread count, last message, and participant display in `conversations.html`. Enhanced backend to fetch and process conversation data for the user.

--- frontend/views.py
+++ frontend/views.py
@@ -1,18 +1,19 @@
+from collections import OrderedDict
+
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_POST
 from django_htmx.http import trigger_client_event
 
 from activities.models import Registration, Activity, grouped_by_response
-from django.utils.translation import gettext_lazy as _
-from collections import OrderedDict
-from django.db.models import Q
-from django.utils import timezone
-
 from frontend.utils import HTMXTemplateResponse
 from members.models import Member
+from messaging.models import Conversation
 from news.models import NewsItem
 
 BUCKETS = OrderedDict(
@@ -25,6 +26,7 @@ BUCKETS = OrderedDict(
     ]
 )
 
+
 @login_required
 def news(request: HttpRequest) -> HttpResponse:
     partial_template = "news"
@@ -32,12 +34,13 @@ def news(request: HttpRequest) -> HttpResponse:
     
     if page_number != 1:
         partial_template = "news_items"
-
+    
     news_items = NewsItem.objects.filter(status=NewsItem.StatusChoices.RELEASED, publish_on__lte=timezone.now()).filter(Q(type=NewsItem.NewsItemTypeChoices.INTERNAL) | Q(type=NewsItem.NewsItemTypeChoices.INTERNAL_EXTERNAL)).order_by("-created")
     paginator = Paginator(news_items, 10)
     page = paginator.get_page(page_number)
     
     return HTMXTemplateResponse(request, "ClubManager/frontend/news.html", {"page": page}, partial_template)
+
 
 def calendar(request: HttpRequest) -> HttpResponse:
     activities = Activity.for_user(request.user, include_registrations=True)
@@ -47,8 +50,9 @@ def calendar(request: HttpRequest) -> HttpResponse:
     for activity in activities:
         if hasattr(activity, "all_registrations") and isinstance(activity.all_registrations, list):
             activity.all_registrations = grouped_by_response(activity.all_registrations, BUCKETS, order_inside_bucket=True)
-            
+    
     return HTMXTemplateResponse(request, "ClubManager/frontend/calendar.html", {"activities": activities}, partial_template="calendar")
+
 
 @login_required
 @require_POST
@@ -65,7 +69,7 @@ def update_registration(request: HttpRequest) -> HttpResponse:
                 return render(request, "ClubManager/frontend/calendar_comment_form.html", {"activity": activity, "member": member, "htmx": False})
             
             return HTMXTemplateResponse(request, "ClubManager/frontend/calendar.html", {"activity": activity, "member": member, "registration": registration, "htmx": True}, partial_template="registration_comment_modal")
-            
+        
         else:
             registration = Activity.update_registration_status_for_member(activity, member, request.user, request.POST.get("response"), comment)
             all_registrations = grouped_by_response(activity.registrations.all(), BUCKETS, order_inside_bucket=True)
@@ -82,18 +86,26 @@ def update_registration(request: HttpRequest) -> HttpResponse:
         
         if not request.htmx:
             return redirect("clubmanager:calendar")
-
+        
         response = HTMXTemplateResponse(request, "ClubManager/frontend/calendar.html", {"activity": activity, "registration": registration, "all_registrations": all_registrations, "swap": True}, partial_template="registration_update")
         return trigger_client_event(response, "registrationsUpdated")
 
-def conversations(request: HttpRequest) -> HttpResponse: ...
+
+def conversations(request: HttpRequest) -> HttpResponse:
+    conversations_for_user = Conversation.objects.for_user(request.user).with_unread_count(request.user).with_last_message().order_by("-last_message_time").prefetch_related("participants", "participants__user")
+    
+    return HTMXTemplateResponse(request, "ClubManager/frontend/conversations.html", {"conversations": conversations_for_user}, partial_template="conversations")
+
 
 def notifications(request: HttpRequest) -> HttpResponse: ...
 
+
 def settings(request: HttpRequest) -> HttpResponse: ...
+
 
 def check(request: HttpRequest) -> HttpResponse:
     return render(request, "ClubManager/base.html#icons")
+
 
 def sidebar_upcoming_activities(request: HttpRequest) -> HttpResponse:
     activities = Activity.for_user(request.user, include_registrations=True).filter(start_time__gte=timezone.now()).order_by("start_time")[:5]
